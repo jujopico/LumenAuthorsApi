d5d9740bbd821a9a9df78f385ad890f4e642512f
jujopico <sujunqt@gmail.com>
2020-01-19 13:48:37 -0500

    create author controller and 5 actions

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * [return the list of authors]
+     * @return Illuuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * [create one new author]
+     * @return Illuuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * [obtain and show one author]
+     * @return Illuuminate\Http\Response
+     */
+    public function show($author)
+    {
+    }
+
+    /**
+     * [updating an existing author]
+     * @return Illuuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+    }
+
+    /**
+     * [remove an existing author]
+     * @return Illuuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+    }  
+
+}
